ae2d23c555f0f8fc2a79cbd582ca303c2a2027e1
Gwahyu290 <gistaw420@gmail.com>
2021-07-06 13:04:53 +0700

    Update cabang fb

--- app/Http/Controllers/FacebookkController.php
+++ app/Http/Controllers/FacebookkController.php
@@ -21,4 +21,31 @@ class FacebookkController extends Controller
         return view('facebookk.index', compact('facebooks'));
         }
     }
+    public function store(Request $request)
+    {
+       $request->validate([
+            'tgl' => 'required|min:3',
+            'cabang_id' => 'required',
+            'link' => 'required',
+        ],[
+            'tgl.required' => 'Alamat Karyawan tidak boleh kosong!!!',
+            'cabang_id.required' => 'Cabang Samchick tidak boleh kosong!!!',
+            'link.required' => 'Link Tugas tidak boleh kosong!!!'
+        ]);
+        // return $request;
+            $cabang_id = Auth()->user()->id;
+            $cabang = DB::select("select * from users where id='$cabang_id'");
+            foreach ($cabang as $c){
+            $cabang_id = $c->cabang_id;
+            }
+            $facebook = new Facebook;
+            $facebook->nama = Auth()->user()->id;
+            $facebook->nama_id = Auth()->user()->name;
+            $facebook->tgl = $request->tgl;
+            $facebook->cabang_id = $request->cabang_id;
+            $facebook->link = $request->link;
+            $facebook->save();
+
+        return redirect('facebookk')->with('status', 'Laporan Upload Facebook Berhasil di Serahkan!!!');
+    }
 }
