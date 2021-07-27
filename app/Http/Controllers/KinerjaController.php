efbcecae6cae3826d3c4f39c32dbf8b805105b1b
khoirulbads <irulad151@gmail.com>
2021-07-27 22:13:03 +0700

    memperbaiki predikat karyawan

--- app/Http/Controllers/KinerjaController.php
+++ app/Http/Controllers/KinerjaController.php
@@ -23,22 +23,23 @@ $data = [];
         $query = "select nama_id as name, SUM(wa+ar+pam) as mingguan, 
                 SUM(fb+ig+gm) as harian,
                 SUM(wa+ar+pam+ig+fb+gm) as bulanan,
+                MONTH(tgl) as bulan, MONTHNAME(tgl) as nmbulan, YEAR(tgl) as tahun,
                 ROUND(
                 (SUM(ig+wa+pam+ar+gm+fb)/
-                ((select count(id) from instagrams where nama_id=name".$tahun1.$bulan1.")+
-                (select count(id) from artikels where nama_id=name".$tahun1.$bulan1.")+
-                (select count(id) from googlemaps where nama_id=name".$tahun1.$bulan1.")+
-                (select count(id) from facebooks where nama_id=name".$tahun1.$bulan1.")+
-                (select count(id) from pamflets where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from whatsapps where nama_id=name ".$tahun1.$bulan1.")
+                ((select count(id) from instagrams where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from artikels where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from googlemaps where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from facebooks where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from pamflets where nama_id=name  and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from whatsapps where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)
                 )),1) as hasil,
-                ((select count(id) from instagrams where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from artikels where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from googlemaps where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from facebooks where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from pamflets where nama_id=name ".$tahun1.$bulan1.")+
-                (select count(id) from whatsapps where nama_id=name ".$tahun1.$bulan1.")
-                ) as jmltugas, MONTH(tgl) as bulan, MONTHNAME(tgl) as nmbulan, YEAR(tgl) as tahun  
+                ((select count(id) from instagrams where nama_id=name  and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from artikels where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from googlemaps where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from facebooks where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from pamflets where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)+
+                (select count(id) from whatsapps where nama_id=name and MONTH(tgl)=bulan and YEAR(tgl)=tahun)
+                ) as jmltugas  
                 from rekap where nama_id='$nama'".$tahun1.$bulan1."group by MONTH(tgl), YEAR(tgl)";
     $data = DB::select($query);
 
